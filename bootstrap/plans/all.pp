ec27c7fdcd1593369d84d63662a7059b60234296
Robert Waffen <rw@example42.com>
2021-10-28 18:26:49 +0200

    add some output for the user
    - reactivate commented plans

--- bootstrap/plans/all.pp
+++ bootstrap/plans/all.pp
@@ -1,25 +1,35 @@
 plan bootstrap::all (
   TargetSpec $targets = ['puppet', 'puppetdb', 'puppetca', 'agent01'],
 ){
-  # prepare puppet agent
+  # install puppet agent
+  out::message('### perpare environment, installing puppet-agent')
   run_task('puppet_agent::install', $targets)
 
   # set hostname
-  # now also set in terraform
   # run_plan('bootstrap::set_hostname', $targets)
 
-  # install locales
+  # install locales to get working environment for yum and postgres
   # run_task('package', $targets, { 'action' => 'install', 'name' => 'glibc-langpack-en' })
   run_task('package', $targets, { 'action' => 'install', 'name' => 'glibc-langpack-de' })
 
-  # bootstrap ca
-  # run_plan('bootstrap::puppetca_01')
-  # run_plan('bootstrap::puppetserver_01')
-  # run_plan('bootstrap::puppetca_02')
-  # run_plan('bootstrap::puppetserver_02')
+  out::message('### initialize PuppetCA - Part 1')
+  run_plan('bootstrap::puppetca_01')
+
+  out::message('### initialize Puppetserver - Part 1')
+  run_plan('bootstrap::puppetserver_01')
+
+  out::message('### initialize PuppetCA - Part 2')
+  run_plan('bootstrap::puppetca_02')
+
+  out::message('### initialize PuppetServer - Part 2')
+  run_plan('bootstrap::puppetserver_02')
+
+  out::message('### initialize PuppetDB')
   run_plan('bootstrap::puppetdb_01')
 
+
   # Disable autosign on puppetca
+  out::message('### All done - disabling autosign')
   run_task('puppet_conf', 'puppetca',
     {
       'action'  => 'set',
